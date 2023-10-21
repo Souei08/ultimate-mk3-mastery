62885f000135ce63ba931e927d4ededf403be44f
Souei08 <jubet.sode.5@gmail.com>
2023-10-21 22:46:33 +0800

    updates

---
+++ app/data/characters.js
@@ -0,0 +1,85 @@
+// Profiles
+import Rain from "../../assets/characters/rain.gif";
+import Reptile from "../../assets/characters/reptile.gif";
+import Striker from "../../assets/characters/stryker.gif";
+import Jax from "../../assets/characters/jax.gif";
+import NightWolf from "../../assets/characters/nightwolf.gif";
+import Jade from "../../assets/characters/jade.gif";
+import Noob from "../../assets/characters/noobsaibot.gif";
+
+import sonya from "../../assets/characters/sonya.gif";
+import kano from "../../assets/characters/kano.gif";
+import mileena from "../../assets/characters/mileena.gif";
+import hidden from "../../assets/characters/hidden.gif";
+import subzeroninja from "../../assets/characters/subzeroninja.gif";
+import subzero from "../../assets/characters/subzero.gif";
+import kunglao from "../../assets/characters/kunglao.gif";
+
+import sektor from "../../assets/characters/sektor.gif";
+import kitana from "../../assets/characters/kitana.gif";
+import ermac from "../../assets/characters/ermac.gif";
+import scorpion from "../../assets/characters/scorpion.gif";
+import cyrax from "../../assets/characters/cyrax.gif";
+
+import kabal from "../../assets/characters/kabal.gif";
+import sindel from "../../assets/characters/sindel.gif";
+import smoke from "../../assets/characters/smoke.gif";
+import liukang from "../../assets/characters/liukang.gif";
+import shangtsung from "../../assets/characters/shangtsung.gif";
+
+// Sprites
+import noobGif from "../../assets/character-sprites/noob.gif";
+import strykerGif from "../../assets/character-sprites/stryker.gif";
+import reptileGif from "../../assets/character-sprites/reptile.gif";
+import jadeGif from "../../assets/character-sprites/jade.gif";
+import nightwolfGif from "../../assets/character-sprites/nightwolf.gif";
+import jaxGif from "../../assets/character-sprites/jax.gif";
+
+export const row1 = [
+  {
+    id: 1,
+    profile: Rain,
+    sprite: noobGif,
+  },
+  {
+    id: 2,
+    profile: Reptile,
+    sprite: reptileGif,
+  },
+  {
+    id: 3,
+    profile: Striker,
+    sprite: strykerGif,
+  },
+  {
+    id: 4,
+    profile: Jax,
+    sprite: jaxGif,
+  },
+  {
+    id: 5,
+    profile: NightWolf,
+    sprite: nightwolfGif,
+  },
+  {
+    id: 6,
+    profile: Jade,
+    sprite: jadeGif,
+  },
+  {
+    id: 7,
+    profile: Noob,
+    sprite: noobGif,
+  },
+];
+export const row2 = [
+  sonya,
+  kano,
+  mileena,
+  hidden,
+  subzeroninja,
+  subzero,
+  kunglao,
+];
+export const row3 = [sektor, kitana, ermac, scorpion, cyrax];
+export const row4 = [kabal, sindel, smoke, liukang, shangtsung];
